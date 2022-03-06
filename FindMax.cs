87799eb6feef7125c778a569ee16c0224e086012
Nirav-12345 <niravpandit0002@gmail.com>
2022-03-06 06:56:45 -0800

    adding Program file

---
+++ FindMax.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GenericFindMax
+{
+    public class FindMax<T> where T : IComparable
+
+    {
+        public T firstvalue, secondvalue, thirdvalue;
+
+
+        public FindMax(T firstvalue, T secondvalue, T thirdvalue)
+        {
+
+            this.firstvalue = firstvalue;
+
+            this.secondvalue = secondvalue;
+
+            this.thirdvalue = thirdvalue;
+
+
+
+
+        }
+
+        
+
+
+
+        public  void  ToPrint(int firstvalue , int secondvalue , int thirdvalue )
+        {
+
+            if (firstvalue.CompareTo(secondvalue) > 0 && firstvalue.CompareTo(thirdvalue) > 0 || firstvalue.CompareTo(secondvalue) >= 0 && firstvalue.CompareTo(secondvalue) > 0 || firstvalue.CompareTo(secondvalue) > 0 && firstvalue.CompareTo(thirdvalue) >= 0)
+            {
+                Console.WriteLine(firstvalue);
+            }
+
+
+
+            if (secondvalue.CompareTo(firstvalue) > 0 && secondvalue.CompareTo(thirdvalue) > 0 || secondvalue.CompareTo(firstvalue) >= 0 && secondvalue.CompareTo(thirdvalue) > 0 || secondvalue.CompareTo(firstvalue) > 0 && secondvalue.CompareTo(thirdvalue) >= 0)
+            {
+                Console.WriteLine(secondvalue);
+            }
+
+            if (thirdvalue.CompareTo(firstvalue) > 0 && thirdvalue.CompareTo(secondvalue) > 0 || thirdvalue.CompareTo(firstvalue) >= 0 && thirdvalue.CompareTo(secondvalue) > 0 || thirdvalue.CompareTo(secondvalue) > 0 && thirdvalue.CompareTo(firstvalue) >= 0)
+            {
+                Console.WriteLine(thirdvalue);
+            }
+
+
+            
+        }
+
+
+
+    }
+
+
+
+
+
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
