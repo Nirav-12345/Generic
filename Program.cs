dcf5a5cb20b1239a7fc6b8809f286aab811c51bb
Nirav-12345 <niravpandit0002@gmail.com>
2022-03-06 07:05:57 -0800

    adding class file

--- Program.cs
+++ Program.cs
@@ -1,19 +1,2 @@
-﻿using System;
-
-namespace RefactGener3_Vars;
-
-class Program
-{
-    public static void Main(string[] args)
-    {
-
-
-
-        
-
-
-        Generic<int> q = new Generic<int>(2,3,4);
-       
-        
-    }
-}
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
