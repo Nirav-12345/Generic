ca7d21ef23cff493354d9824decb9ae24b3ad8f5
Nirav-12345 <niravpandit0002@gmail.com>
2022-03-06 07:04:30 -0800

    adding Program file 3 variables of generic type

---
+++ Generic.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RefactGener3_Vars
+{
+    public class Generic<T> where T:IComparable
+    {
+        public T firstvalue, secondvalue, thirdvalue;
+
+
+        public Generic(T firstvalue, T secondvalue, T thirdvalue)
+        {
+
+            this.firstvalue = firstvalue;
+
+            this.secondvalue = secondvalue;
+
+            this.thirdvalue = thirdvalue;
+        }
+
+
+         public static T Max(T firstvalue, T secondvalue, T thirdvalue)
+            {
+                if (firstvalue.CompareTo(secondvalue) > 0 && firstvalue.CompareTo(thirdvalue) > 0 || firstvalue.CompareTo(secondvalue) >= 0 && firstvalue.CompareTo(secondvalue) > 0 || firstvalue.CompareTo(secondvalue) > 0 && firstvalue.CompareTo(thirdvalue) >= 0)
+                {
+                    return firstvalue;
+                }
+
+
+
+                if (secondvalue.CompareTo(firstvalue) > 0 && secondvalue.CompareTo(thirdvalue) > 0 || secondvalue.CompareTo(firstvalue) >= 0 && secondvalue.CompareTo(thirdvalue) > 0 || secondvalue.CompareTo(firstvalue) > 0 && secondvalue.CompareTo(thirdvalue) >= 0)
+                {
+                    return secondvalue;
+                }
+
+                if (thirdvalue.CompareTo(firstvalue) > 0 && thirdvalue.CompareTo(secondvalue) > 0 || thirdvalue.CompareTo(firstvalue) >= 0 && thirdvalue.CompareTo(secondvalue) > 0 || thirdvalue.CompareTo(secondvalue) > 0 && thirdvalue.CompareTo(firstvalue) >= 0)
+                {
+                    return thirdvalue;
+                }
+
+                return default;
+            }
+
+
+             public T MaxMethod()
+            {
+                T max = Generic<T>.Max(this.firstvalue, this.secondvalue, this.thirdvalue);
+
+                return max;
+            }
+
+
+
+
+
+
+            
+
+
+
+
+
+
+
+
+
+    }
+}
