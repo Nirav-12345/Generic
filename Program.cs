9e80b901dd84fa796f80e16159276fb9ba54d6e6
Nirav-12345 <niravpandit0002@gmail.com>
2022-03-06 06:55:44 -0800

    adding class file

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace GenericFindMax;
+
+class Program
+{
+    public static void Main(String[] args)
+    {
+        FindMax<int> q = new FindMax<int>(2,3,4);
+
+        q.ToPrint(22, 23, 4);
+
+        
+
+
+    }
+}
